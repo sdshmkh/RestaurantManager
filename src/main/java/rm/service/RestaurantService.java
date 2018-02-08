9b5adb53c688e758db7177b4412df9df52409b30
MY-PC\Shahaji <shahaji.deshmukh@stonybrook.edu>
2018-02-08 03:29:59 -0500

    Service layer for restaurant added with finding functionality.

--- src/main/java/rm/service/RestaurantService.java
+++ src/main/java/rm/service/RestaurantService.java
@@ -1,10 +1,21 @@
 package rm.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import rm.dao.RestaurantRepository;
+import rm.model.Menu;
+import rm.model.Restaurant;
 
 /**
  * Created by Shahaji on 08/02/2018.
  */
 @Service
 public class RestaurantService {
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    public Restaurant findByName(Long id) {
+        Restaurant restaurant = restaurantRepository.findOne(id);
+        return restaurant;
+    }
 }
